18e202c3606c3dee07027d14b54f466b67d5d10b
sivakumar <sivakumar.umkc.fall2019@gmail.com>
2020-04-16 12:56:38 -0500

    Webscraping

---
+++ WebScap_Test/WebScraping.py
@@ -0,0 +1,40 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+output = ''
+blacklist = [
+	'[document]',
+	'noscript',
+	'header',
+	'html',
+	'meta',
+	'head',
+	'input',
+	'script',
+    'a'
+    'p'
+	# there may be more elements you don't want, such as "style", etc.
+]
+
+count = 1
+with open("links.txt","r") as hlinks:
+	for url in hlinks:
+		url = url.strip("/\n")
+		res = requests.get(url)
+		html_page = res.content
+		soup = BeautifulSoup(html_page, 'html.parser')
+		text = soup.find_all(text=True)
+
+		for t in text:
+			if t.parent.name not in blacklist:
+	 			output += '{} '.format(t)
+
+		print(output)
+
+		with open("Cancer.txt","a") as file_write:
+			file_write.write(output)
+
+		print("URL:",count,"Completed")
+		count = count +1
+
